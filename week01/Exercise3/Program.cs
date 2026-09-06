724b1df37e2d0a37457ebf3f34d2810cf25350e1
Fernando <fernando.inversionmf@gmail.com>
2026-09-06 10:51:56 -0700

    Update week01 Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.ComponentModel;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomNumber = new Random();
+
+        string playAgain;
+
+
+        do
+        {
+            int magicNumber = randomNumber.Next(1, 101);
+            Console.Write(magicNumber);
+            int guessNumber = -1;
+            int attempts = 0;
+            Console.WriteLine("what is your guess?");
+
+            while (guessNumber != magicNumber)
+            {
+                attempts++;
+
+                guessNumber = int.Parse(Console.ReadLine());
+
+
+                if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine($"congratulations {guessNumber} is the Magic Number, and you guessed them in {attempts} tries");
+            Console.Write("\nDo you want to play again? (yes/no): ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain == "yes" || playAgain == "y");
+
+        Console.WriteLine("\nThanks for playing! Goodbye.");
+
+
     }
+
 }
