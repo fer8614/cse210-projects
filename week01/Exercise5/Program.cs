d68a7eca807f6fc0975abe8ab7569ba07e270e40
Fernando <fernando.inversionmf@gmail.com>
2026-09-06 22:42:13 -0700

    Update week01 Exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string DisplayPersonalMessage()
+        {
+            Console.Write("What is your name? ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int SquareNumber(int number)
+        {
+            return number * number;
+        }
+
+        static void DisplayResult(string userName, int square)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {square}");
+        }
+
+        DisplayWelcome();
+
+        string name = DisplayPersonalMessage();
+        int favoriteNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(favoriteNumber);
+
+        DisplayResult(name, squaredNumber);
+
     }
+
 }
